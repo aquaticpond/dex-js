c34e6ff828643364d35c42a0eadd85e0f72dd1ee
Aquatic Pond <etheriad@gmail.com>
2016-07-06 18:46:06 -0400

    configure validators as a touple (array) of parameters to call on validator instantiation

--- testing/island/dood.js
+++ testing/island/dood.js
@@ -43,7 +43,7 @@ var island = {};
         },
 
         validators: {
-            pants: [new dex.validator('required', () => `${this.property} is a required field.`)],
+            pants: [['required', () => `${this.property} is a required field.`]],
         }
         
     });
