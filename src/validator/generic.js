11a4266a6b45ce7b33ca48cf4ed2d03097378c29
Aquatic Pond <etheriad@gmail.com>
2016-07-18 17:44:51 -0400

    naming the things more betterly

--- src/validator/generic.js
+++ src/validator/generic.js
@@ -1,11 +1,11 @@
 (function(dex, ko){
 
-    let min = (val, min) => val.length > min;
-    let max = (val, max) => val.length < max;
-    let required = (val, isTouched) => !isTouched || (val != '' && typeof val !== 'undefined');
-    let computed = () => false;
+    let min = (val, min) => val && val.length > min;
+    let max = (val, max) => val && val.length < max;
+    let required = (val, isTouched) => /* !isTouched || */ (val != '' && typeof val !== 'undefined');
     let compose = () => false;
+    let test_cakes = function(...args){ /*console.log('testing cakes', this, ...args); */ return false; };
 
-    dex.attach(dex.validator, {min, max, required, computed, compose});
+    dex.attach(dex.validator, {min, max, required, compose, test_cakes});
 
 })(dex, ko);
