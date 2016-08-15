a81bf9ff1e4e02f7a0fb54a2b08525ecc78ba549
Aquatic Pond <etheriad@gmail.com>
2016-08-15 16:21:12 -0400

    password validators

--- src/validator/generic.js
+++ src/validator/generic.js
@@ -6,7 +6,11 @@
     let compose = () => false;
     let test_cakes = function(...cakes){ console.log('testing the cakes', this, ...cakes); return false; };
     let password_match = function(value, password_field){ return value === this.get(password_field); };
+    
+    let has_upper_case = value => /[A-Z]/.test(value);
+    let has_number = value => /[\d]/.test(value);
+    let has_symbol = value => /\W/.test(value);
+    let secure_password = value => has_upper_case(value) && has_number(value) && has_symbol(value);
 
-    dex.attach(dex.validator, {min, max, required, compose, test_cakes, password_match});
-
+    dex.attach(dex.validator, {min, max, required, compose, test_cakes, password_match, has_upper_case, has_number, has_symbol, secure_password});
 })(dex, ko);
