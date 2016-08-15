e5ad9382ff032e2bd00d2836cd1643df6d87256c
Aquatic Pond <etheriad@gmail.com>
2016-08-15 15:54:24 -0400

    password match validator

--- src/validator/generic.js
+++ src/validator/generic.js
@@ -5,7 +5,8 @@
     let required = (val, property) => !property.touched() || (val != '' && typeof val !== 'undefined');
     let compose = () => false;
     let test_cakes = function(...cakes){ console.log('testing the cakes', this, ...cakes); return false; };
+    let password_match = function(value, password_field){ return value === this.get(password_field); };
 
-    dex.attach(dex.validator, {min, max, required, compose, test_cakes});
+    dex.attach(dex.validator, {min, max, required, compose, test_cakes, password_match});
 
 })(dex, ko);
